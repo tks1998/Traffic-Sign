7a070b76559c4101c102a40bb69a1edfa9b9de99
tks1998 <trankimsen1681998@gmail.com>
2020-11-10 10:58:11 +0700

    fix tensorflow

--- Traffic Sign Detector/keras_retinanet/bin/train.py
+++ Traffic Sign Detector/keras_retinanet/bin/train.py
@@ -45,7 +45,7 @@ from ..utils.model import freeze as freeze_model
 
 
 def get_session():
-    config = tf.ConfigProto()
+    config = tf.compat.v1.ConfigProto
     config.gpu_options.allow_growth = True
     return tf.Session(config=config)
 
